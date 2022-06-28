0f98185ae4e09d7d445999c9de89967d146c8b85
gracef45 <g4flik@gmail.com>
2022-06-28 14:18:34 -0500

    Initial Commit

---
+++ clothy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clothy
+//
+//  Created by Grace Flikke24 on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
